0099fc088e2a55bba31d1c36ec20daa97dfe632e
Eugene Rupakov <eugene.rupakov@aurea.com>
2017-11-24 14:39:45 +0300

    Added BTC handling

--- step2/btc/get_balances.php
+++ step2/btc/get_balances.php
@@ -1,3 +1,60 @@
 <?php
-
+/**
+ * Script to extract addresses balances and create raw txs
+ */
 require_once('easybitcoin.php');
+
+const PUBFILENAME = 'publiclist.json';
+const USERNAME = 'taurus';
+const USERPASS = 'Qwerty123';
+const RECIPIENT_ADDRESS = 'myBqKQCepHr6rpsWtTQimyXGFDiV6s41km'; // address to send BTC to
+const TRANSACTION_FEE = 0.3 / 1000; // in mBTCs
+
+$fname = PUBFILENAME;
+if (argc>1) {
+    $fname = $argv[1];
+}
+
+$rfile = @file_get_contents($fname);
+
+if ($rfile===false) {
+    echo 'Addresses list file <$fname> open failed'.PHP_EOL;
+    return;
+}
+
+$resList = [];
+
+$addresses = json_decode($rfile, true);
+
+$bitcoin = new Bitcoin(USERNAME, USERPASS, 'localhost', 18332);
+
+foreach ($addresses as $addr) {
+// process each entry
+    $bal = $bitcoin->getbalance($addr['address']);
+    $addr['balance'] = $bal;
+
+    if ($bal>0) {
+        // list unspent txns
+        $income_tx = $bitcoin->listunspent(1, 99999999, $addr['address']);
+        $input_txs = [];
+        $itx_count = 0;
+
+        foreach ($income_tx as $itx) { // prepare a list of all incoming transactions
+            $k = ['txid'=>$itx['txid'], 'vout'=>$itx_count ];
+            $input_txs[] = $k;
+            $itx_count++;
+        }
+
+        // prepare raw tx
+        $tx_chg = [ RECIPIENT_ADDRESS=>$bal-TRANSACTION_FEE, $addr['address']=>0 ];
+        $rawtx = $bitcoin->createrawtransaction($input_txs, $tx_chg);
+
+        $addr['tx'] = $rawtx;
+
+        // only filled accounts count
+        array_push($resList, $addr);        
+    }
+}
+
+file_put_contents('balances.json', json_encode($resList));
+echo "Success";
---
+++ step3/btc/sign_txs.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Script to sign raw txs
+ * may be called like this: php sign_txs.php [balances.json] [secretlist.json]
+ * 
+ * @author  Eugene Rupakov <eugene.rupakov@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0, AFL-2
+ * @link    http://www.github.com/erupakov
+ */
+
+require_once 'easybitcoin.php';
+
+const TXNFILENAME = 'balances.json';
+const SECFILENAME = 'secretlist.json';
+const USERNAME = 'taurus';
+const USERPASS = 'Qwerty123';
+const BITCOIND_PORT = 18332; // 18332 for testnet, 8332 for mainnet
+
+$txn_fname = TXNFILENAME;
+$sec_fname = SECFILENAME;
+
+if (argc>1) {
+    $txn_fname = $argv[1];
+}
+
+if (argc>2) {
+    $sec_fname = $argv[2];
+}
+
+$txn_file = @file_get_contents($txn_fname);
+
+if ($txn_file===false) {
+    echo 'Transactions list file <$txn_fname> open failed'.PHP_EOL;
+    return;
+}
+
+$sec_file = @file_get_contents($sec_fname);
+if ($sec_file===false) {
+    echo 'Secret keys list file <$sec_fname> open failed'.PHP_EOL;
+    return;
+}
+
+$resList = [];
+
+$txns = json_decode($txn_file, true);
+$pkeys = json_decode($sec_file, true);
+
+$sorted_pvt_keys = [];
+// make indexed array for private keys
+foreach ($pkeys as $p) {
+    $sorted_pvt_keys[$p['address']] = $p['private'];
+}
+
+$bitcoin = new Bitcoin(USERNAME, USERPASS, 'localhost', BITCOIND_PORT);
+
+foreach ($txns as $txn) {
+// process each entry
+    $pk = $sorted_pvt_keys[$txn['address']];
+    $res = $bitcoin->signrawtransaction($txn['tx'], null, [$pk]);
+
+    if ($res['complete']) {
+        $n = ['address'=>$txn['address'], 'tx'=>$res['hex']];
+        array_push($resList, $n);
+    } else {
+        echo 'Error signing TXN for address '.$txn['address'].PHP_EOL;
+        continue;
+    }
+}
+
+file_put_contents('signed.json', json_encode($resList));
+echo "Success";
